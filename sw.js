374f95728d8039255c913e0da30f16bb59443fc3
DaveO280 <154396849+DaveO280@users.noreply.github.com>
2026-05-24 13:00:43 -0400

    Initial deploy of gut protocol tracker for GitHub Pages.

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = 'gut-protocol-v2026-05-21-se4';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './gut-protocol.html',
+        './manifest.json',
+        './sw.js'
+      ]);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      if (cached) return cached;
+      return fetch(event.request).then(function(response) {
+        return response;
+      }).catch(function() {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
